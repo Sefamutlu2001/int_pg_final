33bcedc5c016948b5546d191c8c0a07dab438603
Sefamutlu2001 <sefamutlu39@gmail.com>
2020-06-08 00:54:54 +0300

    intpgfinal

---
+++ Kişi kayıt.php
@@ -0,0 +1,79 @@
+<?php 
+include 'Ust_bilgi.php'
+?>
+
+<div class="bır">
+<form>
+<table>
+
+<tr>
+<td colspan="2" align="center" height="30"><p class="yorumveoneri">Kayıt Alanı</p></td>
+</tr>
+
+
+<tr>
+<td width="300" height="30">Adınızı Giriniz </td>
+<td  width="300"><input class="text" type="text" name="ad" size="38" maxlength="15"></td>
+</tr>
+
+
+<tr>
+<td height="30">Soyadınızı Giriniz </td>
+<td ><input class="text" type="text" name="soy" size="38" maxlength="15"></td>
+</tr>
+
+<tr>
+<td height="30">Kullanıcı Adınızı Giriniz </td>
+<td ><input class="text" type="text" name="soy" size="38" maxlength="15"></td>
+</tr>
+
+<tr>
+<td height="30">Şifrenizi Giriniz </td>
+<td ><input class="text" type="password" name="sifre" size="38" maxlength="10"></td>
+</tr>
+
+
+<tr>
+<td height="30">Mail Adresinizi Giriniz </td>
+<td ><input class="text" type="text" name="mail" size="19" > @ <select class="text"> <option>hotmail.com</option> <option>gmail.com</option> </select></td>
+</tr>
+
+<tr>
+<td height="30">Telefon Numaranızı Giriniz </td>
+<td ><input class="text" type="text" name="soy" size="38" maxlength="15"></td>
+</tr>
+
+<tr>
+
+<table width="600">
+
+<tr>
+
+<td colspan="2" height="30"><input type="submit" value="Kayıt Ol"></td>
+
+<td  height="30"></td>
+
+<td  height="30"> </td>
+
+<td colspan="2"  height="30"><input type="submit" value="Temizle"></td>
+
+<td  height="30"></td>
+
+</tr>
+
+</table>
+
+<td></td>
+</tr>
+
+</table>
+</form>
+</div>
+
+<div class="ıkı"></div>
+
+<div class="uc"></div>
+
+<div class="dort"></div>
+
+<div class="bes"></div>
---
+++ Yorum Ve Öneri.php
@@ -0,0 +1,80 @@
+<?php 
+include 'Ust_bilgi.php'
+?>
+
+<div class="bır">
+
+<form>
+<table>
+
+<tr>
+<td colspan="2" align="center" height="30"><p class="yorumveoneri">Bir cevap yazın </p></td>
+</tr>
+
+<tr>
+<td colspan="2" align="" height="30">E-posta hesabınız yayımlanmayacak.</td>
+</tr>
+
+<tr>
+<td height="30">Yorumunuz</td>
+<td><textarea class="text" cols="40" ></textarea></td>
+</tr>
+
+<tr>
+<td width="300" height="30">Adınızı Giriniz </td>
+<td  width="300"><input class="text" type="text" name="ad" size="38" maxlength="15"></td>
+</tr>
+
+
+<tr>
+<td height="30">Soyadınızı Giriniz </td>
+<td ><input class="text" type="text" name="soy" size="38" maxlength="15"></td>
+</tr>
+
+<tr>
+<td height="30">E-Posta </td>
+<td ><input class="text" type="text" name="mail" size="19" > @ <select class="text"> <option>hotmail.com</option> <option>gmail.com</option> </select></td>
+</tr>
+
+<tr>
+<td height="30">Kullanıcı Adı Giriniz </td>
+<td ><input class="text" type="text" name="soy" size="38" maxlength="15"></td>
+</tr>
+
+<tr>
+<td height="30">Şifre Giriniz </td>
+<td ><input class="text" type="password" name="sifre" size="38" maxlength="10"></td>
+</tr>
+
+<tr>
+<td height="30"><input class="text" type="checkbox" name="" value="yes"> </td>
+<td >Bir dahaki sefere yorum yaptığımda kullanılmak üzere adımı , soyadımı ve e-posta adresimi bu tarayıcıya kaydet.</td>
+</tr>
+
+
+<tr>
+<td height="30"><button>yorumu gönder</button></td>
+<td ></td>
+</tr>
+
+
+</table>
+</form>
+
+
+</div>
+
+<div class="ıkı"></div>
+
+<div class="uc"></div>
+
+<div class="dort"></div>
+
+<div class="bes"></div>
+
+
+
+
+</div>
+</body>
+</html>
